25af5b764b86ca9aac12ecba608897ac9a7164b0
wentaoStudy <2335844083@qq.com>
2020-04-11 14:55:19 +0800

    尝试添加多平台

--- SetPaper.py
+++ SetPaper.py
@@ -1,16 +1,18 @@
 '''
 @Author: wentaoStudy
 @Date: 2020-03-17 22:07:49
-@LastEditTime: 2020-03-18 13:34:07
+@LastEditTime: 2020-04-11 14:55:06
 @LastEditors: wentaoStudy
 @Email: 2335844083@qq.com
 '''
 
 import os
 from PIL import Image
-import win32gui ,win32con, win32api
+import platform
 
-def setWallPaper(filePath):
+#在windows环境下会被执行的代码
+def WindowsSetWallPaper(filePath):
+    import win32gui ,win32con, win32api
     baseFolder = os.getcwd() + "\\" + os.path.dirname(filePath)
     fileName = os.path.basename(filePath).split('.')[0] + '.bmp'
     bmpFile = os.path.join(baseFolder, fileName)
@@ -25,3 +27,14 @@ def setWallPaper(filePath):
     win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, bmpFile, 1+2)
     print(u'成功应用图片:%s为桌面壁纸'  % fileName)
     os.remove('images\\' + fileName)
+
+def setWallPaper(filePath):
+    sysstr = platform.system()
+    #这里默认只在win和mac或Linux环境下使用
+    if(sysstr =="Windows"):
+        WindowsSetWallPaper(filePath)
+    elif(sysstr == "Linux"):
+        pass
+    else:
+        address = os.getcwd() + "/" + WindowsSetWallPaper(filePath) 
+        os.system("osascript -e \"tell application \\\"Finder\\\" to set desktop picture to POSIX file \\\"" + address + "\\\"\"")
